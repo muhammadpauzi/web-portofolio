cbb39b394087d3879debf8dd93252d39e35e42e1
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-09-05 22:39:29 +0700

    fix: tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
     extend: {
       // colors: {
