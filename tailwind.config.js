124b084a912ed4b06dfd3c05793e131cd292f86e
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-09-05 22:36:48 +0700

    fix: tailwind.config path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
